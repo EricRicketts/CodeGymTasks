789ad305f32a66ae296eaa23205dcf3062bc4c21
EricRicketts <eric_ricketts@me.com>
2021-11-05 13:51:44 -0400

    Methods jdk13.task02 task0218 Reptition Is The Mother Of All Learning

---
+++ 1.JavaSyntax/src/en/codegym/task/jdk13/task02/task0218/Solution.java
@@ -0,0 +1,15 @@
+package en.codegym.task.jdk13.task02.task0218;
+
+/* 
+Repetition is the mother of all learning
+*/
+
+public class Solution {
+    public static void print3(String text) {
+        for (int count = 0; count < 3; count++) System.out.println(text);
+    }
+
+    public static void main(String[] args) {
+        print3("I love you!");
+    }
+}
