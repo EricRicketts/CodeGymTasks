89aae6c6b31b01c780d43ac6f9a0fafc946170c6
EricRicketts <eric_ricketts@me.com>
2021-11-05 11:25:23 -0400

    Methods task06 task0606 A Method For All Seasons

---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,42 @@
+package en.codegym.task.pro.task06.task0606;
+
+/* 
+A method for all seasons
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() { // 1
+    }
+
+    public static void universalMethod(int x) { // 2
+    }
+
+    public static void universalMethod(double x) { // 3
+    }
+
+    public static void universalMethod(String x) { // 4
+    }
+
+    public static void universalMethod(boolean x) { // 5
+    }
+
+    public static void universalMethod(int[] x) { // 6
+    }
+
+    public static void universalMethod(double[] x) { // 7
+    }
+
+    public static void universalMethod(String[] x) { // 8
+    }
+
+    public static void universalMethod(int x, int y) { // 9
+    }
+
+    public static void universalMethod(String x, String y) { // 10
+    }
+}
