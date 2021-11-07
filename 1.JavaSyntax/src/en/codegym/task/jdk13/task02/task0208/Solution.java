47ed4f9d773b294e184dd0ee446a103175e193cc
EricRicketts <eric_ricketts@me.com>
2021-11-07 18:55:05 -0500

    Data Types en.codegym.task.jdk13.task02, task0208 Cats Are Good'

---
+++ 1.JavaSyntax/src/en/codegym/task/jdk13/task02/task0208/Solution.java
@@ -0,0 +1,16 @@
+package en.codegym.task.jdk13.task02.task0208;
+
+/* 
+Cats are good
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat kitty = new Cat();
+        Cat tabby = new Cat();
+    }
+
+    public static class Cat {
+
+    }
+}
