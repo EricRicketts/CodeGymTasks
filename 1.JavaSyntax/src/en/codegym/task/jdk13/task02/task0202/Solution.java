b11029c5059f34fedd0b040b60e987ed0ef2a5dc
EricRicketts <eric_ricketts@me.com>
2021-11-07 18:34:57 -0500

    Data Types en.codegym.task.jdk13.task02, task0202 Where Does A Person Come From

---
+++ 1.JavaSyntax/src/en/codegym/task/jdk13/task02/task0202/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task02.task0202;
+
+/* 
+Where does a Person come from?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Person person = new Person();
+    }
+
+    public static class Person {
+        String name;
+        int age;
+        int weight;
+        int money;
+    }
+}
