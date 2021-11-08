160081479dc138649db0ae070b1d97958c464404
EricRicketts <eric_ricketts@me.com>
2021-11-07 19:22:41 -0500

    Data Types en.codegym.task.jdk13.task02, task0212 Crazy Eights

---
+++ 1.JavaSyntax/src/en/codegym/task/jdk13/task02/task0212/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task02.task0212;
+
+/* 
+Crazy eights
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat(), cat2 = new Cat(), cat3 = new Cat(), cat4 = new Cat(), cat5 = new Cat(),
+        cat6 = new Cat(), cat7 = new Cat();
+        Cat cat8 = new Cat();
+        Cat cat9, cat0;
+    }
+
+    public static class Cat {
+
+    }
+}
